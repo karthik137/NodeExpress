c980d8321a4578ccabf609f632daff718884ea3f
kirito <karthik.ganti@mphasis.com>
2018-12-26 10:12:08 +0530

    Callback issue fixed

--- nodejs/node-examples/index.js
+++ nodejs/node-examples/index.js
@@ -1,5 +1,5 @@
 var rect = require('./rectangle');
-var square = require('./square');
+//var square = require('./square');
 /*
 function solveRect(l,b){
     console.log("Solving for rectangle with l = "+l+" and b = "+b);
@@ -13,7 +13,7 @@ function solveRect(l,b){
 }
 */
 
-
+/*
 function solveRect(l,b){
     console.log("Solving for rectangle with l = "+l+" and b = "+b);
     
@@ -27,8 +27,25 @@ function solveRect(l,b){
 
     });
 }
+*/
 
 
+function solveRect(l,b) {
+    console.log("Solving for rectangle with l = "
+                + l + " and b = " + b);
+    rect(l,b, (err,rectangle) => {
+        if (err) {
+	        console.log("ERROR: ", err.message);
+	    }
+        else {
+            console.log("The area of the rectangle of dimensions l = "
+                + l + " and b = " + b + " is " + rectangle.area());
+            console.log("The perimeter of the rectangle of dimensions l = "
+                + l + " and b = " + b + " is " + rectangle.perimeter());
+        }
+    });
+    console.log("This statement after the call to rect()");
+};
 
 
 
--- nodejs/node-examples/rectangle.js
+++ nodejs/node-examples/rectangle.js
@@ -1,21 +1,25 @@
-/*
-exports.perimeter = (x,y) => (2*(x+y))
-exports.area = (x,y) => (x*y)
-*/
 
-module.exports = (x, y, callback) => {
+
+
+module.exports = (x,y,callback) => {
+    console.log("Inside module");
     if (x <=0 && y<=0){
         setTimeout(() => callback(new Error("Dimensions should be greater than zero"), null)
         , 2000);
         //console.log("Dimensions should be greater than zero");
     }else{
+        console.log("Inside else X : "+x+" y : "+y);
         setTimeout(() =>
          callback( null, {
-            perimeter : (x,y) => (2*(x+y)),
-            area : (x,y) => (x*y)
+            perimeter : () => (2*(x+y)),
+            area : () => (x*y)
          })
         , 2000);
     }
 }
 
+/*
+exports.perimeter = (x,y) => (2*(x+y))
+exports.area = (x,y) => (x*y)
+*/
 
