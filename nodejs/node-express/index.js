195c71a7a8eb72373cb8b55433e7fc0774babdf9
unknown <karthik.ganti@mphasis.com>
2018-12-27 12:18:26 +0530

    Express Serve Static Files

--- nodejs/node-express/index.js
+++ nodejs/node-express/index.js
@@ -1,13 +1,17 @@
 const express = require('express');
 const http = require('http');
+const morgan = require('morgan');
 
 const hostname = 'localhost';
 const port = 3000;
 
 const app = express();
+app.use(morgan('dev'));
+app.use(express.static(__dirname + '/public'));
+
 
 app.use((req, res, next) => {
-    console.log(req.headers);
+    //console.log(req.headers);
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/html');
     res.end('<html><body><h1>This is an express server</h1></body></html>');
@@ -18,4 +22,5 @@ const server = http.createServer(app);
 
 server.listen(port, hostname, () => {
     console.log(`Server running at http:${hostname}:${port}`);
+
 });
