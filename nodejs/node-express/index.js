1160baa537e64b53b3466b37bc67341fdc06ee49
kirito <karthik.ganti@mphasis.com>
2018-12-29 11:14:30 +0530

    Added API for dishId

--- nodejs/node-express/index.js
+++ nodejs/node-express/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const http = require('http');
 const morgan = require('morgan');
+const bodyParser = require('body-parser');
 
 const hostname = 'localhost';
 const port = 3000;
@@ -8,6 +9,56 @@ const port = 3000;
 const app = express();
 app.use(morgan('dev'));
 app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.json());
+
+
+app.all('/dishes', (req, res, next) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    next();
+});
+
+app.get('/dishes', (req, res, next) => {
+    console.log("This is dishes get");
+    res.end('Will send all the dishes to you');
+});
+
+app.post('/dishes', (req, res, next) => {
+    res.end('Will add the dish: '+req.body.name +
+     ' With details: '+req.body.description);
+});
+
+app.put('/dishes', (req, res, next) => {
+    res.statusCode = 403;
+    res.end('PUT operation not supported ');
+});
+
+app.delete('/dishes', (req, res, next) => {
+    //res.statusCode = 403;
+    res.end('Deleting all the dishes');
+});
+
+
+
+// with :dishid
+app.get('/dishes/:dishId', (req, res, next) => {
+    res.end('Will send details of the dish: '+req.params.dishId);
+});
+
+app.post('/dishes/:dishId', (req, res, next) => {
+    res.statusCode = 403;
+    res.end('Operation not permitted on dish: '+req.params.dishId);
+});
+
+app.put('/dishes/:dishId', (req, res, next) => {
+    res.write('Updating the dish: '+req.params.dishId);
+    res.end('Will update the dish: '+req.body.name + ' \n With details: '+req.body.description);
+});
+
+app.delete('/dishes/:dishId', (req, res, next) => {
+    //res.statusCode = 403;
+    res.end('Deleting all the dishes');
+});
 
 
 app.use((req, res, next) => {
