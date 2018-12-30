750ae0f98d3776827cc347686ef2783b175991bd
kirito <karthik.ganti@mphasis.com>
2018-12-30 12:21:57 +0530

    Node mongodb exmaple 2

--- nodejs/node-mongo/index.js
+++ nodejs/node-mongo/index.js
@@ -1,10 +1,12 @@
 const mongoclient = require('mongodb').MongoClient;
 
 const assert = require('assert');
+const dboper = require('./mongooperations');
 
 const url = "mongodb://localhost:27017";
 const dbName = 'conFusion';
 
+/*
 mongoclient.connect(url, (err, client) => {
     assert.equal(err, null);
 
@@ -26,6 +28,36 @@ mongoclient.connect(url, (err, client) => {
             db.dropCollection('dishes', (err, result) => {
                 assert.equal(err, null);
                 client.close();
+            });
+        });
+    });
+});
+
+*/
+
+mongoclient.connect(url, (err, client) => {
+    assert.equal(err, null);
+
+    console.log("Connected correctly to the server");
+    const db = client.db(dbName);
+
+    dboper.insertDocument(db, {name: "vadonut", description: "Test"}, 'dishes', (result) => {
+        console.log('Insert Document:\n', result.ops);
+
+        dboper.findDocuments(db, "dishes", (docs) => {
+            console.log("Found documents:\n ", docs);
+
+            dboper.updateDocument(db, {name: 'vadonut'}, {description: 'Updated Test'}, 'dishes', (result) => {
+                console.log("Updated Document:\n ", result.result);
+
+                dboper.findDocuments(db, "dishes", (docs) => {
+                    console.log("Found documents:\n ", docs);
+
+                    db.dropCollection('dishes', (result) => {
+                        console.log("Dropped collection: "+result);
+                        client.close();
+                    });
+                });
             });
         });
     });
