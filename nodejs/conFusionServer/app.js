659600d7719de716faa7c9a28a5388ad867f03e3
unknown <karthik.ganti@mphasis.com>
2019-05-26 14:56:09 +0530

    Https

--- nodejs/conFusionServer/app.js
+++ nodejs/conFusionServer/app.js
@@ -30,6 +30,15 @@ connect.then((db) => {
 });
 
 var app = express();
+app.all('*', (req, res, next) => {
+  if (req.secure) {
+    return next();
+  }
+  else{
+    res.redirect(307, 'https://' + req.hostname + ':' + app.get('secPort') + req.url);  //307 user end use same endpoint
+  }
+});
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
